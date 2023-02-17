7deb310ea5eea723303e67f30b44ed437db33182
Artem <artem.v.mitin@yandex.ru>
2023-02-17 19:54:42 +0300

    feat: add sync tasks

--- src/App.js
+++ src/App.js
@@ -3,29 +3,39 @@ import Form from './components/Form'
 import TodoItem from './components/TodoItem'
 import User from './components/User'
 import Posts from './components/Posts'
+import { useSelector } from 'react-redux'
 
 function App() {
-    return (
-        <div className='min-h-screen h-full w-screen bg-indigo-400'>
-            <div className='container mx-auto px-4'>
-                <header className='flex gap-20 '>
-                    <div className='w-1/3'>
-                        <h1 className='font-bold my-5'>Redux Toolkit State Change</h1>
-                        <User />
-                    </div>
-                    <div className='w-1/3'>
-                        <h1 className='font-bold my-5'>Redux Toolkit Todo App</h1>
-                        <Form />
-                        <TodoItem />
-                    </div>
-                    <div className='w-1/3'>
-                        <h1 className='font-bold my-5'>Redux Toolkit Async Thunk</h1>
-                        <Posts />
-                    </div>
-                </header>
-            </div>
-        </div>
-    )
+  const todos = useSelector((state) => state.todo.todos);
+
+  return (
+    <div className='min-h-screen h-full w-screen bg-indigo-400'>
+      <div className='container mx-auto px-4'>
+        <header className='flex gap-20 '>
+          <div className='w-1/3'>
+            <h2 className='font-bold my-5'>Redux Toolkit State Change</h2>
+            <User />
+          </div>
+          <div className='w-1/3'>
+            <h2 className='font-bold my-5'>Redux Toolkit Todo App</h2>
+            <Form />
+            { todos.length > 0 ? 
+              todos.map((todo) => (
+                <TodoItem
+                  key={todo.id}
+                  todo={todo}
+                />
+              )) : <p className='text-center my-5 font-medium'>Пусто :(</p>
+            }
+          </div>
+          <div className='w-1/3'>
+            <h2 className='font-bold my-5'>Redux Toolkit Async Thunk</h2>
+            <Posts />
+          </div>
+        </header>
+      </div>
+    </div>
+  )
 }
 
 export default App
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,20 +1,34 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { toggleCompletedTodo, deleteTodo } from '../features/todoSlice'
 
-const TodoItem = () => {
-    const completed = false
-    return (
-        <div className='flex justify-between items-center my-2'>
-            <div className='text-sm px-4 py-2 cursor-pointer bg-lime-300 hover:bg-lime-400'>
-                Complete
-            </div>
-            <div className={`text-sm ${completed ? 'line-through font-medium text-lime-400' : ''}`}>
-                Todo Text
-            </div>
-            <div className='text-sm px-4 py-2 flex bg-red-400 hover:bg-red-500 transition-all text-white cursor-pointer'>
-                Delete
-            </div>
-        </div>
-    )
+const TodoItem = ({ todo }) => {
+  const dispatch = useDispatch();
+  const toggleCompletedHandler = (id) => {
+    dispatch(toggleCompletedTodo(id))
+  };
+  const deleteTodoHandler = (id) => {
+    dispatch(deleteTodo(id))
+  };
+  return (
+    <div className='flex justify-between items-center my-2'>
+      <div 
+      className='text-sm px-4 py-2 cursor-pointer bg-lime-300 hover:bg-lime-400'
+      onClick={() => toggleCompletedHandler(todo.id)}
+      >
+        Complete
+      </div>
+      <div className={`text-sm ${todo.completed ? 'line-through font-medium text-lime-400' : ''}`}>
+        {todo.text}
+      </div>
+      <div 
+      className='text-sm px-4 py-2 flex bg-red-400 hover:bg-red-500 transition-all text-white cursor-pointer'
+      onClick={() => deleteTodoHandler(todo.id)}
+      >
+        Delete
+      </div>
+    </div>
+  )
 }
 
 export default TodoItem
